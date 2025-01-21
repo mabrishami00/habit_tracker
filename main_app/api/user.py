b8fb1c91f3fdcb5ca3b02b6748276f5445e2f20a
Mahdi <mabrishami00@gmail.com>
2025-01-21 22:19:55 +0330

    Add UserGetNewTokens view for generating new JWT access and refresh tokens

--- main_app/api/user.py
+++ main_app/api/user.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.generics import CreateAPIView
 
 from seriailzers.user import UserSerializer
+from core.authentication import JWTAuthentication
 
 
 class UserRegisterView(CreateAPIView):
@@ -16,4 +17,21 @@ class UserLoginView(APIView):
         username, password = request.data.get("username"), request.data.get("password")
         if authenticate(username=username, password=password):
             return Response({"result": True}, status=status.HTTP_200_OK)
+        return Response({"result": False}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class UserGetNewTokens(APIView):
+    def post(self, request):
+        access_token, refresh_token = (
+            JWTAuthentication().get_new_access_token_and_refresh_token(request)
+        )
+        if access_token and refresh_token:
+            return Response(
+                {
+                    "result": True,
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                },
+                status=status.HTTP_200_OK,
+            )
         return Response({"result": False}, status=status.HTTP_401_UNAUTHORIZED)

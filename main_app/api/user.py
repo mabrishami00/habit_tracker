9816e0964b7bdceb8af1f533cbc52839554503ba
Mahdi <mabrishami00@gmail.com>
2025-01-21 22:17:08 +0330

    Add UserLoginView for user authentication endpoint

--- main_app/api/user.py
+++ main_app/api/user.py
@@ -11,3 +11,9 @@ class UserRegisterView(CreateAPIView):
     serializer_class = UserSerializer
 
 
+class UserLoginView(APIView):
+    def post(self, request):
+        username, password = request.data.get("username"), request.data.get("password")
+        if authenticate(username=username, password=password):
+            return Response({"result": True}, status=status.HTTP_200_OK)
+        return Response({"result": False}, status=status.HTTP_401_UNAUTHORIZED)

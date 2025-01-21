6b831c58fe8942d80801989c02196e9301c1f5ef
Mahdi <mabrishami00@gmail.com>
2025-01-21 20:08:07 +0330

    Add Habit model to main_app with fields for tracking user habits

---
+++ main_app/models/habit.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Habit(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    target = models.IntegerField()
+    times_completed = models.IntegerField(default=0)
+    done = models.BooleanField(default=False)
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title

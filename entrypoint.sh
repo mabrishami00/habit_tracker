d63afd314389f120d0192c6cfc5dfb85a3171e5d
Mahdi <mabrishami00@gmail.com>
2025-01-22 22:40:13 +0330

    Add PostgreSQL wait logic to entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,18 @@
 set -e
 set -x
 
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 if [ "$1" = "web" ]; then
     python3 manage.py migrate --noinput
     python3 manage.py collectstatic --noinput

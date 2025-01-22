154638b324cee688d6ba25210d6e61993b029463
Mahdi <mabrishami00@gmail.com>
2025-01-22 22:04:29 +0330

    Update database settings to use PostgreSQL with environment variables

--- Config/settings.py
+++ Config/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "Config.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
+        'PORT': config('POSTGRES_PORT', default='5432'),
     }
 }
 

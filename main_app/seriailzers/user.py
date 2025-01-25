cca66c0fb1aac64ddc455848117967dc2616b922
Mahdi <mabrishami00@gmail.com>
2025-01-25 22:45:47 +0330

    Implement custom User model with email as username and update UserSerializer for ModelSerializer usage

--- main_app/seriailzers/user.py
+++ main_app/seriailzers/user.py
@@ -4,9 +4,12 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class UserSerializer(serializers.Serializer):
+class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
         fields = ["username", "password", "email"]
         extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
